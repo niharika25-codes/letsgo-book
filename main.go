6e793d462ab5a8fea49ef87b12130a45eef822a1
niharika <niharika.pendem@gmail.com>
2026-02-02 21:30:03 +0530

    c2.2

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// handler function
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// router or servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
+	log.Print("starting server on :4000")
+
+	// server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }

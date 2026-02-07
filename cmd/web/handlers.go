e610d27acd3aef00f8e44e2167e77d89abbe73ea
niharika25-codes <niharika.pendem@gmail.com>
2026-02-07 19:36:56 +0000

    ch2.8

---
+++ cmd/web/handlers.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+// handler function
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("server", "go")
+	files := []string {
+		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	w.Write([]byte("Hello from Snippetbox"))
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet ID %d ...", id)
+	
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// router or servemux
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+
+	log.Print("starting server on :4000")
+
+	// server
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
